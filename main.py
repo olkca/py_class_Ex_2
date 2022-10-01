d53256cd206cd0d32b5ee223ed4455b319a4d4b5
Shapoval <olekcandrsapoval41@gmail.com>
2022-10-01 10:42:49 +0300

    Update: Ex_5

---
+++ main.py
@@ -0,0 +1,25 @@
+start = int(input("Ведіть перше число --->"))
+end = int(input("Ведіть друге число --->"))
+while start < end:
+    if start % 2 == 1:
+      print(start)
+      start += 2
+    if start % 2 == 0:
+        start = start + 1
+    while start < end:
+        if start % 2 == 1:
+            print(start)
+            start += 2
+while start > end:
+    start = num
+    end = start
+    end = num
+    if start % 2 == 1:
+      print(start)
+      start += 2
+    if start % 2 == 0:
+        start = start + 1
+    while start < end:
+        if start % 2 == 1:
+            print(start)
+            start += 2
